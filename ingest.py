a3457f6c438b8dfbe6abe3da553b79578488c5db
Tori Champagne <champagnet@moravian.edu>
2026-04-08 21:23:38 -0400

    Update ingest.py to ingest federal document data

--- ingest.py
+++ ingest.py
@@ -3,6 +3,7 @@ from .utils.sql import connect as connect_sql
 from .utils.ingest_comment import insert_comment
 from .utils.ingest_docket import insert_docket
 from .utils.ingest_document import insert_document
+from .utils.ingest_federal_document import insert_federal_document
 from .utils.ingest_opensearch import ingest_comment_from_text as insert_comment_os
 from .utils.ingest_opensearch import ingest_extracted_text_from_text as insert_extracted_text_os
 from .utils.ingest_summary import insert_summary
@@ -20,17 +21,21 @@ def ingest_comment_sql(contents):
     sql.commit()
     sql.close()
 
-
 def ingest_extracted_text(contents):
     os = connect_opensearch()
     insert_extracted_text_os(os,contents)
-
 
 def ingest_document(contents):
     sql = connect_sql()
     insert_document(sql, contents)
     sql.commit()
     sql.close()
+
+def ingest_federal_document(contents):   
+    sql = connect_sql()                                                                                                       
+    insert_federal_document(sql, contents)
+    sql.commit()                                                                                                              
+    sql.close() 
 
 def ingest_docket(contents):
     sql = connect_sql()
